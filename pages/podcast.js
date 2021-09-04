ce48152ede84412885b14ede17f23c1c49da3d24
Lisset Cuevas <lissetcuevas@lyft.com>
2021-09-04 14:45:56 -0500

    Delete Podcast page old data

--- pages/podcast.js
+++ pages/podcast.js
@@ -1,11 +1,7 @@
 import Head from 'next/head'
 import StaticNavbar from './components/navbar'
 import Footer from './components/footer'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from '../styles/sections/CirculoLectura.module.css';
 
 import React from 'react'
 
@@ -23,40 +19,9 @@ export default function Podcast(props){
 
       <StaticNavbar/>
       <main>
-        <div className={styles.currentBook}>
-          <Container>
-            <Row>
-              <Col xs={12} md={7} className={styles.smHide}>
-                <h1>{currentBook.title}</h1>
-                <h2>{currentBook.author}</h2>
-                <p>{currentBook.description}</p>
-                <a href="https://forms.gle/GwTu8se6xxNGTgw99">Lee con nosotras</a>
-              </Col>
-              <Col xs={12} md={5} className="text-center">
-                <img className={styles.backFloresCirculo} src="/images/flores.svg"></img>
-                <span>
-                <img src={currentBook.image}></img>  
-                </span>
-                <h4>Este mes</h4>  
-              </Col>
-            </Row>
-          </Container>
-        </div>
+        <h1>Podcast</h1>
       </main>
       <Footer/>
     </div>
   )
 }
-
-export const getStaticProps = async () => {
-
-  const fs = require("fs");
-  const yaml = require('js-yaml');
-
-  const rawContent = fs.readFileSync('site/content/books.yml', 'utf8');
-  const books = yaml.load(rawContent);
-      
-  return {
-    props: { books }
-  };
-};
